3b57d14a19ad70fd716bc862274b1bedf79aae41
John <thejohncjordan@gmail.com>
2021-01-27 07:05:59 -0600

    Project completed

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 from tkinter import *
 
 window = Tk()
-window.title("My first GUI Program")
-window.minsize(width=500, height=300)
+window.title("Mile to Km Converter")
+window.minsize(width=260, height=100)
 
-my_label = Label(text="I am a label", font=("Ariel", 24, "bold"))
-my_label.grid(column=0, row=0)
+label1 = Label(text="Miles")
+label1.grid(column=2, row=0)
 
-my_label.config(text="New Text")
+label2 = Label(text="is equal to: ")
+label2.grid(column=0, row=1)
+
+label3 = Label(text="Km")
+label3.grid(column=2, row=1)
+
+label4 = Label(text="0")
+label4.grid(column=1, row=1)
+
+
 
 def button_clicked():
-    change_label = input.get()
-    my_label.config(text=change_label)
+    change_label = int(input.get())*2.2
+    label4.config(text=change_label)
 
 
-button = Button(text="Click Me", command=lambda: button_clicked())
-button.grid(column=1, row=1)
-
-button2 = Button(text="Button2")
-button2.grid(column=2, row=0)
+button = Button(text="Calculate", command=lambda: button_clicked())
+button.grid(column=1, row=2)
 
 input = Entry(width=10)
-input.grid(column=3, row=2)
+input.grid(column=1, row=0)
 
 
 
